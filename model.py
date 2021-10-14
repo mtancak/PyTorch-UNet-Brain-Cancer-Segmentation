2ab707584366e4910ec13e5b9d5ddb6a468f8112
Milan Tancak <tancak.milan@gmail.com>
2021-10-14 13:17:27 +0100

    added a template for the constructor

--- model.py
+++ model.py
@@ -5,13 +5,36 @@ import torch.nn as nn
 class UNET(nn.Module):
     def __init__(
             self, 
-            in_channels = 4,  # number of channels: t1, t1ce, t2, flair
-            out_channels = 4,  # number of classes: background, edema, non-enhancing, GD-enhancing
-            layer_features = [64, 128, 256, 512]):  # default for UNET
+            in_c = 4,  # number of channels: t1, t1ce, t2, flair
+            out_c = 4,  # number of classes: background, edema, non-enhancing, GD-enhancing
+            n_f = [64, 128, 256, 512]):  # default for UNET
         super(UNET, self).__init__()
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.layer_features = layer_features
+        
+        # initialise member vars
+        self.in_channels = in_c
+        self.out_channels = out_c
+        self.n_features = n_f
+        
+        # generate layers
+        self.contracting_layers = nn.ModuleList()
+        self.bottleneck_layers = nn.ModuleList()
+        self.expanding_layers = nn.ModuleList()
+        
+        # generate contracting layers
+        for n in self.n_features:
+            print("v " + str(n))
+        
+        # generate contracting layers
+        for n in reversed(self.n_features):
+            print("^ " + str(n))
+        
+        # compresses numerous feature channels down to one channel per class
+        self.output_layer = nn.Conv3d(
+            in_channels = self.n_features[0], 
+            out_channels = self.out_channels, 
+            kernel_size = 1, 
+            stride = 1, 
+            bias = False)
 
 
 if __name__ == "__main__":
