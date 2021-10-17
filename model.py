5238ba4707e966166437931c253677eee8651819
Milan Tancak <tancak.milan@gmail.com>
2021-10-17 11:15:51 +0100

    fixed some of the variable names and formatting

--- model.py
+++ model.py
@@ -3,39 +3,39 @@ import torch.nn as nn
 
 
 # returns a modulelist used for a single layer of a UNET architecture
-def double_conv_3d(in_f, out_f):
+def double_conv_3d(in_features, out_features):
     return nn.ModuleList([
         nn.Conv3d(
-            in_channels=in_f, 
-            out_channels=out_f,
-            kernel_size=3, 
-            stride=1, 
-            padding=1, 
-            bias=False),
-        nn.BatchNorm3d(out_f),
+            in_channels = in_features, 
+            out_channels = out_features,
+            kernel_size = 3, 
+            stride = 1, 
+            padding = 1, 
+            bias = False),
+        nn.BatchNorm3d(out_features),
         nn.ReLU(inplace=True),
-        nn.Conv3d(in_channels=out_f, 
-                  out_channels=out_f, 
-                  kernel_size=3, 
-                  stride=1, 
-                  padding=1, 
-                  bias=False),
-        nn.BatchNorm3d(out_f),
+        nn.Conv3d(in_channels = out_features, 
+                  out_channels = out_features, 
+                  kernel_size = 3, 
+                  stride = 1, 
+                  padding = 1, 
+                  bias = False),
+        nn.BatchNorm3d(out_features),
         nn.ReLU(inplace=True)
     ])
 
 class UNet3D(nn.Module):
     def __init__(
             self, 
-            in_c = 4,  # number of channels: t1, t1ce, t2, flair
-            out_c = 4,  # number of classes: background, edema, non-enhancing, GD-enhancing
-            n_f = [64, 128, 256, 512]):  # default for UNET
+            in_channels = 4,  # number of channels: t1, t1ce, t2, flair
+            out_channels = 4,  # number of classes: background, edema, non-enhancing, GD-enhancing
+            n_features = [64, 128, 256, 512]):  # default for UNET
         super(UNet3D, self).__init__()
         
         # initialise member vars
-        self.in_channels = in_c
-        self.out_channels = out_c
-        self.n_features = n_f
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.n_features = n_features
         
         # generate layers
         self.contracting_layers = nn.ModuleList()
