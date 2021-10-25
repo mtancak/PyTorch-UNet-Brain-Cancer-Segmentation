fa8c98ae434d360c326fc18d3aebe744b054d162
Milan Tancak <tancak.milan@gmail.com>
2021-10-25 20:43:07 +0100

    added f1 score metric and loss

--- train.py
+++ train.py
@@ -16,20 +16,32 @@ LEARNING_RATE = 0.0001
 BATCH_SIZE = 1
 NUMBER_OF_EPOCHS = 100
 
+def onehot_initialization_v2(a, ncols=4):
+    out = torch.zeros(a.numel(), ncols)
+    out[torch.arange(a.numel()), a.ravel()] = 1
+    return out.to(device=DEVICE)
 
-# prints out predictions
-def print_predictions(model, loader):
+def f1(probability, targets):
+    probability = nn.Sigmoid()(probability.flatten())
+    targets = targets.flatten()
+    assert (probability.shape == targets.shape)
+
+    intersection = 2.0 * (probability * targets).sum()
+    union = (probability * probability).sum() + (targets * targets).sum()
+    dice_score = intersection / union
+    return 1.0 - dice_score
+
+# measures the f1 score of predictions at the end of an epoch
+def f1_metric(model, loader):
+    f1_score = 0.0
     with torch.no_grad():
         for data, seg in loader:
             pred = model(data)
-            pred = torch.argmax(pred, dim=1)
+            f1_score += f1(pred, onehot_initialization_v2(seg))
 
-            plt.imshow(data[0][0][20].detach().cpu().numpy())
-            plt.show()
-            plt.imshow(pred[0][20].detach().cpu().numpy())
-            plt.show()
-            plt.imshow(seg[0][20].detach().cpu().numpy())
-            plt.show()
+    f1_score /= len(loader)
+
+    return f1_score
 
 # measures accuracy of predictions at the end of an epoch (bad for semantic segmentation)
 def accuracy(model, loader, prin=False):
@@ -46,35 +58,19 @@ def accuracy(model, loader, prin=False):
 
     return (correct_pixels / total_pixels).item()
 
-# measures the f1 score of predictions at the end of an epoch
-def f1(model, loader):
-    f1_score = 0.0
-    with torch.no_grad():
-        for data, seg in loader:
-            pred = model(data)
-            pred = torch.argmax(pred, dim=1)
-            f1_class_scores = []
-            for c in range(4):  # 4 class, can't use np.unique as some entries might not have all 4 classes
-                tp = (pred == c).sum() + 1e-5
-                fp = (pred == c).sum() * (seg != c).sum() + 1e-5
-                fn = (pred != c).sum() * (seg == c).sum() + 1e-5
-                precision = tp / (tp + fp)
-                recall = tp / (tp + fn)
-                f1_class_scores.append(((2.0 * precision * recall) / (precision + recall)).item())
-            f1_score += np.average(np.array(f1_class_scores))
-
-    f1_score /= len(loader)
-
-    return f1_score
-
 # a training loop that runs a number of training epochs on a model
 def train(model, loss_function, optimizer, train_loader, validation_loader):
     for epoch in range(NUMBER_OF_EPOCHS):
         model.train()
         progress = tqdm(train_loader)
         
-        for batch, (data, seg), in enumerate(progress):
+        for batch, (data, seg2), in enumerate(progress):
             pred = model(data)
+            seg = onehot_initialization_v2(seg2)
+            seg = torch.reshape(seg, (1, 64, 64, 64, 4))
+            seg = torch.moveaxis(seg, 4, 1)
+            print("pred shape = " + str(pred.shape))
+            print("seg shape = " + str(seg.shape))
             loss = loss_function(pred, seg)
 
             # make the progress bar display loss
@@ -87,40 +83,52 @@ def train(model, loss_function, optimizer, train_loader, validation_loader):
 
         model.eval()
         print("Accuracy for epoch (" + str(epoch) + ") is: " + str(accuracy(model, validation_loader)))
-        print("F1 Score for epoch (" + str(epoch) + ") is: " + str(f1(model, validation_loader)))
+        print("F1 Score for epoch (" + str(epoch) + ") is: " + str(f1_metric(model, validation_loader)))
+        plt.imshow(data[0][0][20].detach().cpu().numpy())
+        plt.show()
+        plt.imshow(torch.argmax(pred, dim=1)[0][20].detach().cpu().numpy())
+        plt.show()
+        plt.imshow(seg2[0][20].detach().cpu().numpy())
+        plt.show()
+        plt.imshow(data[0][0][20].detach().cpu().numpy())
+        plt.show()
+        plt.imshow(torch.argmax(pred, dim=1)[0][20].detach().cpu().numpy())
+        plt.show()
+        plt.imshow(seg2[0][20].detach().cpu().numpy())
+        plt.show()
     print("Final Accuracy for epoch is: " + str(accuracy(model, validation_loader)))
-    print("Final F1 Score for epoch is: " + str(f1(model, validation_loader)))
+    print("Final DSC Score for epoch is: " + str(f1_metric(model, validation_loader)))
     print("Print: ")
-    print_predictions(model, validation_loader)
+    # print_predictions(model, validation_loader)
 
 
 if __name__ == "__main__":
     # create model
-    model = UNet3D(in_channels = 4, out_channels = 4).to(DEVICE)
+    model = UNet3D(in_channels=4, out_channels=4).to(DEVICE)
 
-    loss_function = nn.CrossEntropyLoss()
-    optimizer = optim.Adam(model.parameters(), lr = LEARNING_RATE)
-    
+    loss_function = f1
+    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
     train_dataset = BraTS20Dataset(
-        set_dir = "C:/Users/Milan/Documents/Fast_Datasets/BraTS20/prep/train/",
-        data_dir = "data/",
-        seg_dir = "mask/")
-    
+        set_dir="C:/Users/Milan/Documents/Fast_Datasets/BraTS20/prep/train/",
+        data_dir="data/",
+        seg_dir="mask/")
+
     validation_dataset = BraTS20Dataset(
-        set_dir = "C:/Users/Milan/Documents/Fast_Datasets/BraTS20/prep/val/",
-        data_dir = "data/",
-        seg_dir = "mask/")
-    
+        set_dir="C:/Users/Milan/Documents/Fast_Datasets/BraTS20/prep/val/",
+        data_dir="data/",
+        seg_dir="mask/")
+
     train_loader = DataLoader(
         train_dataset,
-        batch_size = BATCH_SIZE,
-        num_workers = 0,
-        shuffle = True)
+        batch_size=BATCH_SIZE,
+        num_workers=0,
+        shuffle=True)
 
     validation_loader = DataLoader(
         validation_dataset,
-        batch_size = 1,
-        num_workers = 0,
-        shuffle = False)
+        batch_size=1,
+        num_workers=0,
+        shuffle=False)
 
     train(model, loss_function, optimizer, train_loader, validation_loader)
